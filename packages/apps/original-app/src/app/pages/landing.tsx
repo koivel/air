532ffc84649db9fb1829ec487b24f93d17ede701
eclipseop <guest173800@gmail.com>
2022-05-26 17:14:28 -0500

    remove log

--- packages/apps/original-app/src/app/pages/landing.tsx
+++ packages/apps/original-app/src/app/pages/landing.tsx
@@ -42,7 +42,6 @@ const ButtonGroup = ({loggedIn}: {loggedIn: boolean}) => {
 
 export function Landing() {
   const { user } = useKAuth();
-  console.log(user);
 
   return (
     <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
